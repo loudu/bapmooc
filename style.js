d8b7f478b2c1528252e044e7bed863bf92dee610
Lou Duménil <loud18@hotmail.fr>
2015-12-19 19:56:57 +0100

    Re modification
    j’ai pas réussi à remettre des étoile du coup la c’est des carrés

--- style.js
+++ style.js
@@ -1,7 +1,16 @@
-var logID = 'log',
-  log = $('<div id="'+logID+'"></div>');
-$('body').append(log);
-  $('[type*="radio"]').change(function () {
-    var me = $(this);
-    log.html(me.attr('value'));
+jQuery(document).ready(function($) {
+  $('.rating .star').hover(function() {
+    $(this).addClass('to_rate');
+    $(this).parent().find('.star:lt(' + $(this).index() + ')').addClass('to_rate');
+    $(this).parent().find('.star:gt(' + $(this).index() + ')').addClass('no_to_rate');
+  }).mouseout(function() {
+    $(this).parent().find('.star').removeClass('to_rate');
+    $(this).parent().find('.star:gt(' + $(this).index() + ')').removeClass('no_to_rate');
+  }).click(function() {
+    $(this).removeClass('to_rate').addClass('rated');
+    $(this).parent().find('.star:lt(' + $(this).index() + ')').removeClass('to_rate').addClass('rated');
+    $(this).parent().find('.star:gt(' + $(this).index() + ')').removeClass('no_to_rate').removeClass('rated');
+    /*Save your rate*/
+    /*TODO*/
   });
+});
